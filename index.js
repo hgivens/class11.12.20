404054660d6290b7492f749c0306685dd7645f68
GIVENS <hg0899537@otc.edu>
2020-11-17 11:28:23 -0600

    finished story

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const chalkAnimation = require('chalk-animation');
 // Combine styled and normal strings
 log(chalk.red('Hello let me tell you a story') + ' about a little old woman' + chalk.blue(' who had a big scare'));
 log(chalk.green('In reality she was just crazy') + ' a cazy old little lady' + chalk.green(' the world never ended'));
-
-chalkAnimation.glitch('soon the world will end... she thought is 2012 is was porjected in the "aincent text" that every one obsess over');
+log(chalk.cyan('Every thing was fine in the end a her life went on and on and on.'));
+chalkAnimation.glitch('soon there would be more like her in 2020');
 
